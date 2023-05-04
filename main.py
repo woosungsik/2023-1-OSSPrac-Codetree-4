7dfe4767e03156c46a2486e5c45da7d306f6ad20
Youth-o <2021111495@dgu.ac.kr>
2023-05-04 15:33:23 +0900

    예제처럼 templates에 있는 파일 새로 생성했고, main.py 기본틀 만들었습니다.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, render_templates, request
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return('Hello World! \n CodeTree 입니다.')
+
 if __name__ == '__main__':
     app.run()

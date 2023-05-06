0a47c84d299633d5e185b7f730aea8bdcc2aec1f
ChaeyoonC <codbs@dgu.ac.kr>
2023-05-06 16:58:13 +0900

    (main.py, result.html) main 전달 오류 수정 완료했습니다.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect,render_template,request
 
 app=Flask(__name__)
+data = dict()
 
 @app.route('/', methods=['GET','POST'])
 def main():
@@ -9,24 +10,17 @@ def main():
 @app.route('/result', methods=['GET','POST'])
 def result():
     if request.method == 'POST':
-        Students = {}
-        result['Name'] = request.form.get('name')
-        result['StudentNumber'] = request.form.get('StudentNumber')
-        result['Major'] = request.form.get('major')
-        result['Email'] = request.form.get('email_id') + '@' + request.form.get('email_addr')
-        result['Gender'] = request.form.get('gender')
-        result['ProgrammingLanguages'] = ", ".join(request.form.getlist('ProgrammingLanguages'))
-    
-        Students[StudentNumber] = {
-            'name': name,
-            'major': major,
-            'email': email_id + '@' + email_addr,
-            'gender': gender,
-            'ProgrammingLanguages': ProgrammingLanguages
-        }
+        Students = dict()
+        Students["name"] = request.form.get('name')
+        Students["StudentNumber"] = int(request.form.get('StudentNumber'))
+        Students["major"] = request.form.get('major')
+        Students["email"] = request.form.get('email_id') + '@' + request.form.get('email_addr')
+        Students["gender"] = request.form.get('gender')
+        Students["ProgrammingLanguages"] = ", ".join(request.form.getlist('ProgrammingLanguages'))
         
-        sorted_students = sorted(students.items(), reverse = True)
-        return render_template('result.html', Students =  sorted_students)
+        data[Students["StudentNumber"]] = Students
+        result = [data[key] for key in sorted(data)]
+        return render_template('result.html', result =  result)
     
 if __name__=='__main__':
     app.run(debug=True)

7dc174ec238356e14e515182cdae6aa30bcc9152
Youth-o <2021111495@dgu.ac.kr>
2023-05-06 16:36:27 +0900

    (main.py) @app.route('/result') 의 전달하는 부분 오류 고쳤습니다.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect,render_template,request
 
 app=Flask(__name__)
+
 @app.route('/', methods=['GET','POST'])
 def main():
     return render_template('main.html')
@@ -8,14 +9,24 @@ def main():
 @app.route('/result', methods=['GET','POST'])
 def result():
     if request.method == 'POST':
-        result = dict()
+        Students = {}
         result['Name'] = request.form.get('name')
         result['StudentNumber'] = request.form.get('StudentNumber')
         result['Major'] = request.form.get('major')
         result['Email'] = request.form.get('email_id') + '@' + request.form.get('email_addr')
         result['Gender'] = request.form.get('gender')
-        result['ProgrammingLanguages'] = request.form.getlist('ProgrammingLanguages')
-        return render_template('result.html', result = result)
-
+        result['ProgrammingLanguages'] = ", ".join(request.form.getlist('ProgrammingLanguages'))
+    
+        Students[StudentNumber] = {
+            'name': name,
+            'major': major,
+            'email': email_id + '@' + email_addr,
+            'gender': gender,
+            'ProgrammingLanguages': ProgrammingLanguages
+        }
+        
+        sorted_students = sorted(students.items(), reverse = True)
+        return render_template('result.html', Students =  sorted_students)
+    
 if __name__=='__main__':
     app.run(debug=True)
